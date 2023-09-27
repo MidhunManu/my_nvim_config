8284ce7e334d8a9fb3ec97d383f06bed5606079c
MidhunManu <midhunmanu384@gmail.com>
2023-09-27 16:45:41 +0530

    great nvim

---
+++ lua/midhun/init.lua
@@ -0,0 +1,22 @@
+require("midhun.remap")
+
+-- my vim configs
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.mouse = ""
